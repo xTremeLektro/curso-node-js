9ea48f9ccb89654c5ee557f91ad909266048ed7f
xTremeLektro <sergio.agustin.cabrera@gmail.com>
2025-05-30 20:02:34 -0400

    terminado module 1

--- clase-1/1.os-info.js
+++ clase-1/1.os-info.js
@@ -10,3 +10,4 @@ console.log('CPUs', os.cpus())
 console.log('Memoria libre', os.freemem() / 1024 / 1024)
 console.log('Memoria total', os.totalmem() / 1024 / 1024)
 console.log('uptime', os.uptime() / 60 / 60)
+    
